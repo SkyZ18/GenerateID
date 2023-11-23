5970ca5ff883311d445b52adce6a9b9f78e2d0e8
SkyZ18 <114467762+SkyZ18@users.noreply.github.com>
2023-11-23 13:58:30 +0100

    Added Constant class

---
+++ src/main/java/org/example/Constans.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public interface Constans {
+    String LIST_OF_NUMS = "0123456789";
+    String FILENAME = "savedIds.txt";
+    String INIT_ID = "00000000%n";
+    int LENGTH = 8;
+}
